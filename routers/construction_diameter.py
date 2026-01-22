512b497d75684f8868fcec1e75a797710348c60b
Ayrat <kiamovajrat1@gmail.com>
2026-01-22 23:57:59 +0300

    add routers get and delete, make migration for table construction_diameter

--- routers/construction_diameter.py
+++ routers/construction_diameter.py
@@ -1,11 +1,39 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from database.data import SessionDep
+from models.well import Well
 from utils.calculations.casing_diameter import calculate_diameters
 from schemas.construction_diameter import Diameter
+from sqlalchemy import select, delete
+from models.construction_diameter import ConstructionDiameter
 
 router = APIRouter()
 
-@router.post("/diameter",
-            tags=["diameter"])
-def get_construction_diameters(session: SessionDep, well_id: int, first_diameter: int) -> list[Diameter]:
+@router.post("/diameter/{well_id}",
+            tags=["diameter"],
+             response_model=list[Diameter])
+def calculate_construction_diameters(session: SessionDep, well_id: int, first_diameter: int) -> list[Diameter]:
     return calculate_diameters(session=session, well_id=well_id, first_diameter=first_diameter)
+
+@router.get("/diameter/{well_id}",
+            tags=["diameter"],
+            response_model=list[Diameter])
+def get_construction_diameters(session: SessionDep, well_id: int) -> list[Diameter]:
+    well = session.get(Well, well_id)
+    if well is None:
+        raise HTTPException(status_code=404, detail="Well not found")
+    stmt = select(ConstructionDiameter).where(ConstructionDiameter.well_id == well.id)
+    diameters_db = session.scalars(stmt).all()
+    if not diameters_db:
+        raise HTTPException(status_code=404, detail="Diameters not found")
+    return [Diameter.model_validate(diameters) for diameters in diameters_db]
+
+@router.delete("/diameter/{well_id}",
+               tags=["diameter"])
+def delete_construction_diameters(session: SessionDep, well_id: int):
+    well = session.get(Well, well_id)
+    if well is None:
+        raise HTTPException(status_code=404, detail="Well not found")
+    stmt = delete(ConstructionDiameter).where(ConstructionDiameter.well_id == well.id)
+    result = session.execute(stmt)
+    session.commit()
+    return {"success": True}
